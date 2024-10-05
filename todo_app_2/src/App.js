ac3e8704415a5bba8a87b27cfce5d1e9454a8163
Tejas <90674200+Pat-il@users.noreply.github.com>
2024-10-05 10:32:06 +0530

    Using Props Passing Data

---
+++ todo_app_2/src/App.js
@@ -0,0 +1,33 @@
+import AppName from './components/AppName';
+import AddTodo from './components/AddTodo';
+import TodoItems from './components/TodoItems';
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./App.css";
+
+function App() {
+
+  const todoItems = [
+    {
+      name : "Buy Milk",
+      dueDate : "4/10/2023"
+    },
+    {
+      name : "Go to College",
+      dueDate : "4/10/2023"
+    }
+  ]
+
+  return (
+    
+      <center className="todo-container">
+        <AppName />
+        <AddTodo />
+        <TodoItems todoItems={todoItems}/>  
+        
+      </center>
+    
+  );
+}
+
+export default App;
+
