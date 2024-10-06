bc605c7fae77235792a80d52c803e45a76fbb5af
Tejas <90674200+Pat-il@users.noreply.github.com>
2024-10-06 22:26:07 +0530

    Using useState hook

--- todo_app_3/src/App.js
+++ todo_app_3/src/App.js
@@ -8,31 +8,17 @@ import { useState } from 'react';
 
 function App() {
 
-  const initialTodoItems = [
-    {
-      name : "Buy Milk",
-      dueDate : "4/10/2023"
-    },
-    {
-      name : "Go to College",
-      dueDate : "4/10/2023"
-    },
-    {
-      name : "Like The Video",
-      dueDate : "Right Now"
-    }
-  ]
-
-  const [todoItems, setTodoItems] = useState(initialTodoItems);
+  const [todoItems, setTodoItems] = useState([]);
 
   const handleNewItem = (itemName, itemDueDate) => {
     console.log(`New Item Added ${itemName} at ${itemDueDate}`);
-    const setItems = [...initialTodoItems, 
+    const newTodoItems = [...todoItems, 
       {
       name : itemName,
       dueDate : itemDueDate
     }];
-    setTodoItems(setItems);
+    setTodoItems(newTodoItems);
+    
   }
 
   const handleDeletedItem = (itemName) => {
@@ -56,3 +42,41 @@ function App() {
 
 export default App;
 
+/* import AppName from "./components/AppName";
+import AddTodo from "./components/AddTodo";
+import TodoItems from "./components/TodoItems";
+import WelcomeMessage from "./components/WelcomeMessage";
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [todoItems, setTodoItems] = useState([]);
+
+  const handleNewItem = (itemName, itemDueDate) => {
+    console.log(`New Item Added: ${itemName} Date:${itemDueDate}`);
+    const newTodoItems = [
+      ...todoItems,
+      { name: itemName, dueDate: itemDueDate },
+    ];
+    setTodoItems(newTodoItems);
+  };
+
+  const handleDeleteItem = (todoItemName) => {
+    const newTodoItems = todoItems.filter((item) => item.name !== todoItemName);
+    setTodoItems(newTodoItems);
+  };
+
+  return (
+    <center className="todo-container">
+      <AppName />
+      <AddTodo onNewItem={handleNewItem} />
+      {todoItems.length === 0 && <WelcomeMessage></WelcomeMessage>}
+      <TodoItems
+        todoItems={todoItems}
+        onDeleteClick={handleDeleteItem}
+      ></TodoItems>
+    </center>
+  );
+}
+
+export default App; */
